5f85bc2daa1c8e809cc63aa233a68da98c4bd963
Laura <lbe@ucaya.com>
2025-09-01 12:17:48 +0200

    Uodate path

--- src/Components/photography.tsx
+++ src/Components/photography.tsx
@@ -1,4 +1,8 @@
+import type React from "react"
+import './photography.css'
+
 export const Photography: React.FC = () => {
+  const BASE_URL = 'portfolio';
   return (
     <section className="photographie">
     <div className="padding"></div>
@@ -7,56 +11,56 @@ export const Photography: React.FC = () => {
       <div>
           <div className="flex">
               <div className="img1">
-                  <img src="/images/machine.JPG" alt="" />
+                  <img src={`${BASE_URL}/images/machine.JPG`} alt="" />
               </div>
               <div className="img2">
-                  <img src="/images/urbex09.JPG" alt="" />
+                  <img src={`${BASE_URL}/images/urbex09.JPG`} alt="" />
               </div>
           </div>
 
           <div className="flex">
             <div className="img3">
-              <img src="/images/roue.jpg" alt="" />
+              <img src={`${BASE_URL}/images/roue.jpg`} alt="" />
             </div>
             <div className="img4">
-              <img src="/images/IMG_8230.JPG" alt="" />
+              <img src={`${BASE_URL}/images/IMG_8230.JPG`} alt="" />
             </div>
           </div>
 
 
           <div className="flex">
               <div className="img7">
-                  <img src="/images/feuille2.JPG" alt="" />
+                  <img src={`${BASE_URL}/images/feuille2.JPG`} alt="" />
               </div>
               <div className="img8">
-                  <img src="/images/urbex01.jpg" alt="" />
+                  <img src={`${BASE_URL}/images/urbex01.jpg`} alt="" />
               </div>
           </div>
 
           <div className="flex">
               <div className="img9">
-                  <img  src="/images/kaya.JPG" alt="" />
+                  <img  src={`${BASE_URL}/images/kaya.JPG`} alt="" />
               </div>
               <div className="img10">
-                  <img src="/images/biche.jpg" alt="" />
+                  <img src={`${BASE_URL}/images/biche.jpg`} alt="" />
               </div>
           </div>
 
           <div className="flex">
               <div className="img11">
-                  <img src="/images/pont.jpg" alt="" />
+                  <img src={`${BASE_URL}/images/pont.jpg`} alt="" />
               </div>
               <div className="img12">
-                  <img src="/images/flou.JPG" alt="" />
+                  <img src={`${BASE_URL}/images/flou.JPG`} alt="" />
               </div>
           </div>
 
           <div className="flex">
               <div className="img13">
-                  <img src="/images/fenetre.JPG" alt="Fenêtre" />
+                  <img src={`${BASE_URL}/images/fenetre.JPG`} alt="Fenêtre" />
               </div>
               <div className="img14">
-                  <img src="/images/chloe.jpg" alt="Chloé" />
+                  <img src={`${BASE_URL}/images/chloe.jpg`} alt="Chloé" />
               </div>
           </div>
       </div>
--- src/Components/projects.tsx
+++ src/Components/projects.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import './projects.css'
 
 export const Projects: React.FC = () => {
+  const BASE_URL = 'portfolio';
   return (
     <section className="projets">
       <div className="padding"></div>
@@ -10,17 +11,17 @@ export const Projects: React.FC = () => {
       <div className="padding"></div>
       <div className="flex-rectangle">
         <div className="capt5">
-          <img src="/images/Capture06.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture06.png`} alt="" />
         </div>
       </div>
       <div className="flex-rectangle">
         <div className="capt6">
-          <img src="/images/Capture07.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture07.png`} alt="" />
         </div>
       </div>
       <div className="flex-rectangle">
         <div className="capt7">
-          <img src="/images/Capture08.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture08.png`} alt="" />
           <p>HTML, CSS - <a href="https://hiisplot.github.io/beacon/" target="_blank">VOIR SUR GITHUB</a></p>
           <p>Projet personnel - Maquette Beacon</p>
         </div>
@@ -35,19 +36,19 @@ export const Projects: React.FC = () => {
 
       <div className="flex-rectangle">
         <div className="capt8">
-          <img src="/images/Capture09.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture09.png`} alt="" />
         </div>
       </div>
 
       <div className="flex-rectangle">
         <div className="capt9">
-          <img src="/images/Capture10.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture10.png`} alt="" />
         </div>
       </div>
 
       <div className="flex-rectangle">
         <div className="capt10">
-          <img src="/images/Capture11.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture11.png` alt="" />
           <p>HTML, CSS - <a href="https://hiisplot.github.io/mountain" target="_blank">VOIR SUR GITHUB</a></p>
           <p>Projet personnel - Maquette La montagne</p>
         </div>
@@ -62,19 +63,19 @@ export const Projects: React.FC = () => {
 
       <div className="flex-rectangle">
         <div className="capt8">
-          <img src="/images/Capture12.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture12.png`} alt="" />
         </div>
       </div>
 
       <div className="flex-rectangle">
         <div className="capt9">
-          <img src="/images/Capture13.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture13.png`} alt="" />
         </div>
       </div>
 
       <div className="flex-rectangle">
         <div className="capt10">
-          <img src="/images/Capture14.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture14.png`} alt="" />
           <p>HTML, CSS  - <a href="https://hiisplot.github.io/the-resto" target="_blank">VOIR SUR GITHUB</a></p>
           <p>Projet personnel - Maquette "The resto"</p>
         </div>
@@ -90,13 +91,13 @@ export const Projects: React.FC = () => {
 
       <div className="flex">
         <div className="capt1">
-          <img src="/images/Capture01.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture01.png`} alt="" />
           <p>HTML, CSS, Javascript  - <a href="https://hiisplot.github.io/Projet-memory" target="_blank">VOIR SUR GITHUB</a></p>
           <p>Projet ENI - Jeu du memory</p>
         </div>
 
         <div className="capt2">
-          <img src="/images/Capture05.png" alt="" />
+          <img src={`${BASE_URL}/images/Capture05.png`} alt="" />
         </div>
       </div>
 
